ebec9f1e24df294d1650c4c6da85f8261b1cd4ef
Marat Maltcev <MaltcevMS@gmail.com>
2020-02-06 12:57:56 +0100

    MBUS-1: Added GameFactory.Service project, implemented basic events and handlers

---
+++ GameFactoryService/Events/IssueAdded.cs
@@ -0,0 +1,14 @@
+﻿using GameFactory.Domain.Issue;
+
+namespace GameFactory.Service.Events
+{
+    public class IssueAdded
+    {
+        public IssueAdded(IIssue issue)
+        {
+            Issue = issue;
+        }
+
+        public IIssue Issue { get; set; }
+    }
+}
---
+++ GameFactoryService/Events/IssueStatusChanged.cs
@@ -0,0 +1,16 @@
+﻿using GameFactory.Domain.Issue;
+
+namespace GameFactory.Service.Events
+{
+    public class IssueStatusChanged
+    {
+        public IIssue Issue { get;}
+        public IssueStatuses NewStatus { get; }
+
+        public IssueStatusChanged(IIssue issue, IssueStatuses newStatus)
+        {
+            Issue = issue;
+            NewStatus = newStatus;
+        }
+    }
+}
