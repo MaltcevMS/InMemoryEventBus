296c3697bdcc2e8a8ae06a78b7c3e9149398c7f3
Marat Maltcev <MaltcevMS@gmail.com>
2020-02-06 13:01:46 +0100

    MBUS-1: Added initial implementation of the console application, added console logger

---
+++ Common.Infrastructure/IGameLogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Common.Infrastructure
+{
+    public interface IGameLogger
+    {
+        void Info(string logMessage);
+        void Error(string errorMessage);
+        void Error(string errorMessage, Exception exception);
+    }
+}
--- InMemoryEventBus/Program.cs
+++ InMemoryEventBus/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Common.Infrastructure;
+using GameFactory.Domain.Issue;
+using GameFactory.Domain.IssuePipeline;
+using GameFactory.Service.Events;
+using GameFactory.Service.Handlers;
 using InMemoryEventBus.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,7 +14,21 @@ namespace InMemoryEventBus
     {
         static void Main(string[] args)
         {
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IEventBus, EventBus>()
+                .AddSingleton<IPipelineManager, PipelineManager>()
+                .AddSingleton<IGameLogger, ConsoleGameLogger>()
+                .AddSingleton<GameDeveloper>()
+                .AddSingleton<GameStatusLogger>()
+                .BuildServiceProvider();
 
+            var eventBus = serviceProvider.GetService<IEventBus>();
+            eventBus.Subscribe<GameDeveloper>();
+            eventBus.Subscribe<GameStatusLogger>();
+
+            Task.Run(() => eventBus.PublishAsync(new IssueAdded(new Feature("Game menu", 2))));
+
+            Console.ReadLine();
         }
     }
 }
